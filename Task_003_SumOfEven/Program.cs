225546ac1b0f5771277be697dfaed3639b2f9a47
RenatBax <renchik_125.111@mail.ru>
2021-12-13 13:12:55 +0300

    Задача по нахождению суммы четных и нечетных чисел из 15

---
+++ Task_003_SumOfEven/Program.cs
@@ -0,0 +1,12 @@
+﻿// Сумма четных чисел из 15
+
+int sumOfEven = 0, sumOfOdd = 0, i = 1;
+while (i <= 15)
+{
+    if (i%2 == 0) sumOfEven = sumOfEven + i; 
+    
+    if (i%2 != 0) sumOfOdd = sumOfOdd + i; // для нечетных
+    i++;
+}
+Console.WriteLine(sumOfEven);
+Console.WriteLine(sumOfOdd);
