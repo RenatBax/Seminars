b2d31e386bdb98e446f1d9c3e90878d7b7282287
RenatBax <renchik_125.111@mail.ru>
2021-12-13 18:14:18 +0300

    Задача по нахождению периметра треугольника

---
+++ Task_004_291021_Perimetr/Program.cs
@@ -0,0 +1,14 @@
+﻿// Нахождение периметра треугольника
+
+int Perimetr(int x, int y)
+{
+    return 2 * (x + y);
+}
+
+int a, b;
+Console.Write("a: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Perimetr(a, b));
