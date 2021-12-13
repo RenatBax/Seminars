2161c336af5a1c8b04d9d7ecd679c4923e30d1d6
RenatBax <renchik_125.111@mail.ru>
2021-12-13 19:13:25 +0300

    Задача по обнулению массива

---
+++ Task_007_ZeroArray/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача по обнулению массива
+
+// int[] array;
+// array = new int[10];
+
+int[] array = {4, 4, 6, 3, 8, 7, 9, 2, 22, 5};
+// первый способ
+int i = 0;
+for(i = 0; i < 10; i++)
+{
+    Console.WriteLine(array[i]);
+}
+// while (i < 10)
+// {
+//     array[i] = 0;
+//     Console.WriteLine(array[i]);
+//     i++;
+// }
+
+// второй способ
+int j = 0;
+for(j = 0; j < 10; j++)
+{
+    array[j] = 0;
+}
+
+for(j = 0; j < 10; j++)
+{
+    Console.Write(array[j] + " ");
+}
