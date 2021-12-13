fc8fcf071632ced76de4790aef4150dc56445f41
RenatBax <renchik_125.111@mail.ru>
2021-12-13 18:33:21 +0300

    Задача по нахождению минимального из двух

---
+++ Task_005_Min2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Нахождение минимального из 2
+
+int min(int x, int y)
+{
+    if (x > y) return y;
+    else return x;
+}
+
+int a, b;
+Console.Write("a: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное из двух, равно " + min(a, b));
