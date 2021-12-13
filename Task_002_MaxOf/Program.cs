c8ceeec6721867cfc09860d85c3a9a7ebb5b4be4
RenatBax <renchik_125.111@mail.ru>
2021-12-13 12:43:43 +0300

    Задача по нахождению максимального из двух

---
+++ Task_002_MaxOf/Program.cs
@@ -0,0 +1,13 @@
+﻿// Максимальное из двух
+
+int a, b, max;
+Console.Write("a: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b: ");
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    max = a;
+}
+else max = b;
+Console.WriteLine("Максимальное из двух, равно " +max);
