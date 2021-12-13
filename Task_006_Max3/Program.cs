2b08923373e818170d03d4912465121074bc4c91
RenatBax <renchik_125.111@mail.ru>
2021-12-13 18:54:58 +0300

    Задача по нахождению максимального из трех

---
+++ Task_006_Max3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Нахождение махимального из 3
+
+int maxOfThree(int a, int b, int c)
+{
+    if (a > b && a > c) return a;
+    if (b > a && b > c) return b;
+    else return c;
+}
+int a, b, c;
+Console.WriteLine("Введите первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное из трех, равно " +maxOfThree(a, b, c));
