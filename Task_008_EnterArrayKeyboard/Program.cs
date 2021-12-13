827a5cdf342d4679b9862bff62ef7d4fbd07e06c
RenatBax <renchik_125.111@mail.ru>
2021-12-13 19:45:35 +0300

    Задача по заполнению массива с клавиатуры и случайным образом

---
+++ Task_008_EnterArrayKeyboard/Program.cs
@@ -0,0 +1,17 @@
+﻿// ВВод масивы с клавиатуры
+
+int[] array = new int[10];
+
+for(int i = 0; i < 10; i++)
+{
+    string s = Console.ReadLine();
+    array[i] = Convert.ToInt32(s);
+}
+
+for(int j = 0; j < 10; j++)
+{
+    Console.Write(array[j] +" ");
+}
+
+Console.WriteLine();
+
---
+++ Task_009_RandomArray/Program.cs
@@ -0,0 +1,15 @@
+﻿// Заполнение массива случайными числами
+
+
+int[] arrayRandom = new int[10];
+
+//Random rnd = new Random().Next(0, 100);
+for(int i = 0; i < 10; i++)
+{
+    arrayRandom[i] = new Random().Next(0, 100);
+}
+
+for(int i = 0; i < 10; i++)
+{
+    Console.Write(arrayRandom[i] +" ");
+}
