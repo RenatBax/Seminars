82fece440dd2c5e4f2bd746415112140127c4500
RenatBax <renchik_125.111@mail.ru>
2021-12-13 12:19:14 +0300

    Задача по нахождению среднеарифметического числа из четырех

---
+++ Task_001_ArithmetiMeanOfFour/Program.cs
@@ -0,0 +1,14 @@
+﻿// Среднеарифметическое четырех чисел
+double a, b, c, d, S;
+
+Console.Write("a: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("b: ");
+b = Convert.ToDouble(Console.ReadLine());
+Console.Write("c: ");
+c = Convert.ToDouble(Console.ReadLine());
+Console.Write("d: ");
+d = Convert.ToDouble(Console.ReadLine());
+
+S = (a  + b + c + d) / 4;
+Console.WriteLine($"Среднеарифметическое из четырех чисел, равно: {S} ");
